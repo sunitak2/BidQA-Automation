5520c7c60af5893e664efddbf9d4b529528ccb6b
sunitak2 <30784662+sunitak2@users.noreply.github.com>
2017-08-06 17:52:28 -0700

    Update BidQA_Home_Test.java

--- test/java/com/QAUber/BidQA_Home_Test.java
+++ test/java/com/QAUber/BidQA_Home_Test.java
@@ -29,42 +29,55 @@ public class BidQA_Home_Test {
         //About Us link
         page.getBidQAHome().BidQAAboutUs().click();
         Thread.sleep(3000);
+        //Verify that title is About us
         Assert.assertEquals("About Us",driver.findElement(By.xpath("//*[@id='wrapper']/div[2]/div/div[1]/div[1]")).getText());
         System.out.println("Found 'About Us'");
 
         //For QA Professional link
         page.getBidQAHome().BidQA_QAProfessional().click();
         Thread.sleep(3000);
+        
+        //Verify that title is 'For QA Professionals'
         Assert.assertEquals("For QA Professionals",driver.findElement(By.xpath("//*[@id=\"wrapper\"]/div[2]/div/div[1]/div[1]")).getText());
         System.out.println("Found 'QA Professional'");
 
         // For Project Owners link
         page.getBidQAHome().BidQA_ForProjectOwners().click();
         Thread.sleep(3000);
+        
+        //Verify that title is 'For Project Owners'
         Assert.assertEquals("For Project Owners",driver.findElement(By.xpath("//*[@id=\"wrapper\"]/div[2]/div/div[1]/div[1]")).getText());
         System.out.println("Found 'For Project Owner'");
 
         // Q&A link
         page.getBidQAHome().BidQA_QA().click();
         Thread.sleep(3000);
+        
+        //Verify that title is 'Q&A'
         Assert.assertEquals("Q&A",driver.findElement(By.xpath("//*[@id=\"wrapper\"]/div[2]/div/div[1]/div[1]")).getText());
         System.out.println("Found 'Q&A' ");
 
         //Support link
         page.getBidQAHome().BidQA_Support().click();
         Thread.sleep(3000);
+        
+        //Verify that title is 'Support'
         Assert.assertEquals("Support",driver.findElement(By.xpath("//*[@id=\"wrapper\"]/div[2]/div/div[1]/div[1]")).getText());
         System.out.println("Found 'Support' ");
 
         //Register link
         page.getBidQAHome().BidQA_Register().click();
         Thread.sleep(3000);
+        
+        //Verify that user is on 'Register - BidQA'
         Assert.assertEquals("Register - BidQA",driver.findElement(By.xpath("//*[@id=\"wrapper\"]/div[2]/div/div")).getText());
         System.out.println("Found 'Register' ");
 
         //Login link
         page.getBidQAHome().BidQA_Login().click();
         Thread.sleep(3000);
+        
+        //Verify that user is on Login page
         Assert.assertEquals("Login - BidQA",driver.findElement(By.xpath("//*[@id=\"wrapper\"]/div[2]/div/div")).getText());
         System.out.println("Found 'Login' ");
 
